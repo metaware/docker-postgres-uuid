9c366db8a7061d70ca089d9d024bcbe7c203d2e3
Jasdeep <narang.jasdeep@gmail.com>
2018-12-14 20:57:04 -0500

    add new extensions

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM postgres:10.5
+FROM postgres:10.6
 
 MAINTAINER Jasdeep Singh
 
--- createExtension.sh
+++ createExtension.sh
@@ -2,4 +2,7 @@
 
 gosu postgres postgres --single <<- EOSQL
    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+   CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+   CREATE EXTENSION IF NOT EXISTS "hstore";
+   CREATE EXTENSION IF NOT EXISTS "plpgsql";
 EOSQL
