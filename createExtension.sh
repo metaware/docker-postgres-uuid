fd2c41adbc3e735c8de054238dfeeaffa7d04b81
Jasdeep <narang.jasdeep@gmail.com>
2018-12-14 23:57:24 -0500

    more additions

--- createExtension.sh
+++ createExtension.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+   CREATE SCHEMA IF NOT EXISTS shared_extensions;
    CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA shared_extensions;
    CREATE EXTENSION IF NOT EXISTS "pgcrypto" SCHEMA shared_extensions;
    CREATE EXTENSION IF NOT EXISTS "hstore" SCHEMA shared_extensions;
    CREATE EXTENSION IF NOT EXISTS "plpgsql" SCHEMA shared_extensions;
+   GRANT USAGE ON SCHEMA shared_extensions TO public;
 EOSQL
