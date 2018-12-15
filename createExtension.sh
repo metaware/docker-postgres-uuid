f5532b05258aaa81a3a6a76c5f71690abf5df82a
Jasdeep <narang.jasdeep@gmail.com>
2018-12-15 01:46:14 -0500

    bootstrap change

--- createExtension.sh
+++ createExtension.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# CREATE SCHEMA IF NOT EXISTS shared_extensions;
+# GRANT USAGE ON SCHEMA shared_extensions TO public;
+# GRANT EXECUTE ON ALL functions IN SCHEMA shared_extensions TO public;
+# ALTER DEFAULT PRIVILEGES IN SCHEMA shared_extensions GRANT EXECUTE ON functions TO public;
+# ALTER DEFAULT PRIVILEGES IN SCHEMA shared_extensions GRANT USAGE ON types TO public;
+
+# CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA shared_extensions;
+# CREATE EXTENSION IF NOT EXISTS "pgcrypto" SCHEMA shared_extensions;
+# CREATE EXTENSION IF NOT EXISTS "hstore" SCHEMA shared_extensions;
+# CREATE EXTENSION IF NOT EXISTS "plpgsql" SCHEMA shared_extensions;
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
    CREATE SCHEMA IF NOT EXISTS shared_extensions;
-   GRANT USAGE ON SCHEMA shared_extensions TO public;
-   GRANT EXECUTE ON ALL functions IN SCHEMA shared_extensions TO public;
-   ALTER DEFAULT PRIVILEGES IN SCHEMA shared_extensions GRANT EXECUTE ON functions TO public;
-   ALTER DEFAULT PRIVILEGES IN SCHEMA shared_extensions GRANT USAGE ON types TO public;
-   
-   CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA shared_extensions;
-   CREATE EXTENSION IF NOT EXISTS "pgcrypto" SCHEMA shared_extensions;
-   CREATE EXTENSION IF NOT EXISTS "hstore" SCHEMA shared_extensions;
-   CREATE EXTENSION IF NOT EXISTS "plpgsql" SCHEMA shared_extensions;
+   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+   CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+   CREATE EXTENSION IF NOT EXISTS "hstore";
+   CREATE EXTENSION IF NOT EXISTS "plpgsql";
 EOSQL
